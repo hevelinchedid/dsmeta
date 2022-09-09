415c502d5bfbc269705dcf2075aed819998752e7
Hevelin Chedid <hevi.kimetsu@gmail.com>
2022-09-09 11:01:52 -0300

    Sale listing

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -17,7 +17,7 @@ function SalesCard() {
 
     const [sales, setSales] = useState<Sale[]>([]);
     useEffect(() => {
-        axios.get('${BASE_URL}/sales')
+        axios.get(`${BASE_URL}/sales`)
             .then(response => {
                 setSales(response.data.content)
             })
@@ -64,12 +64,12 @@ function SalesCard() {
                             sales.map(sale => {
                                 return (
                                     <tr key={sale.id}>
-                                        <td className="show992">sale.id</td>
-                                        <td className="show576">08/07/2022</td>
+                                        <td className="show992">{sale.id}</td>
+                                        <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
                                         <td>{sale.sellerName}</td>
-                                        <td className="show992">15</td>
-                                        <td className="show992">11</td>
-                                        <td>R$ 55300.00</td>
+                                        <td className="show992">{sale.visited}</td>
+                                        <td className="show992">{sale.deals}</td>
+                                        <td>R${sale.amount.toFixed(2)}</td>
                                         <td>
                                             <div className="dsmeta-red-btn-container">
                                                 <NotificationButton />
